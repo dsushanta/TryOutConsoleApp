53390096aa6412aa5f676511f399286d8cc1b9ab
dsushanta <dasgupta.sushanta@gmail.com>
2019-06-26 12:27:41 +0530

    commiting changes

--- TryOutConsoleApp/Program.cs
+++ TryOutConsoleApp/Program.cs
@@ -7,6 +7,9 @@ namespace TryOutConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Tutorial t = new Tutorial(1, "C# Tutorial");
+            Console.WriteLine(t);
         }
     }
 }
---
+++ TryOutConsoleApp/Tutorial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryOutConsoleApp
+{
+    class Tutorial
+    {
+        int tutorialId;
+        string tutorialName;
+
+        public Tutorial() {
+        }
+        public Tutorial(int tutorialId, string tutorialName) {
+            this.tutorialId = tutorialId;
+            this.tutorialName = tutorialName;
+        }
+        public int getTutorialId() {
+            return tutorialId;
+        }
+        public string getTutorialName(string name) {
+            return tutorialName;
+        }
+        public void setTutorialId(int id) {
+            tutorialId = id;
+        }
+        public void setTutorialName(string name) {
+            tutorialName = name;
+        }
+        public override string ToString() {
+            return tutorialId + " : " + tutorialName;
+        }
+    }
+}
